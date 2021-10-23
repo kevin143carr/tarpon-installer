fd0cfdb469f871f8e33fa65cd65f48da01a664d3
kevin143carr <kevin143carr@yahoo.com>
2021-10-23 12:19:23 +0000

    Add files via upload
    Initial Commit

---
+++ tarpon_installer.py
@@ -0,0 +1,115 @@
+import sys
+import time
+from configparser import ConfigParser
+from task import Task
+import os.path
+from os import path
+
+configfile = "config.ini"
+version = "2.5.2"
+
+class iniInfo:
+    username = ""
+    password = ""
+    host = ""
+    buildtype = ""
+    installtype = ""
+    resources = ""
+    files = dict()
+    repo = dict()
+    rpms = dict()
+    actions = dict()
+    modify = dict()
+    finalactions = dict()
+
+    def __init__(self):
+        config_object = ConfigParser()
+        config_object.read(configfile)
+        userinfo = config_object["USERINFO"]
+        serverinfo = config_object["SERVERCONFIG"]
+        build = config_object["BUILD"]
+        self.resources = build["resources"]
+        self.files = config_object._sections['FILES']
+        self.username = userinfo["username"]
+        self.password = userinfo["password"]
+        self.host = serverinfo["host"]
+        self.buildtype = build["buildtype"]
+        self.installtype = build["installtype"]
+        self.repo = config_object._sections['REPO']
+        self.rpms = config_object._sections['RPM']
+        self.actions = config_object._sections['ACTIONS']
+        self.modify =  config_object._sections['MODIFY']
+        self.finalactions = config_object._sections['FINAL']
+
+class mainClass:
+
+    def main(self):
+        print("******************************************************************")
+        print("******************************************************************")
+        print(" ><###> Tarpon Installer <###>< is an open source install creator.")
+        print(" It has been made open source under the MIT Licensing agreement.")
+        print(" Feel free to use, modify and distribute")
+        print(" as needed, as long as this banner remains in place")
+        print("*  VERSION {}".format(version))
+        print("******************************************************************")
+        print("******************************************************************")
+        time.sleep(2)
+        ini_info = iniInfo()
+        task = Task(ini_info.username, ini_info.password, ini_info.host, ini_info.resources)
+
+        # if Remote Type then login via SSH
+        if ini_info.installtype == 'REMOTE':
+            task.loginSSH()
+
+        # Repos and RPMs are Linux only
+        if ini_info.buildtype == 'LINUX':
+            task.installRepo(ini_info.resources, ini_info.repo)
+            task.installRPMs(ini_info.resources, ini_info.rpms)
+
+        task.copyFromResources(ini_info.resources, ini_info.files, ini_info.installtype, ini_info.buildtype)
+        task.doActions(ini_info.actions, ini_info.installtype, ini_info.buildtype)
+        task.modifyFiles(ini_info.modify)
+        task.finalActions(ini_info.finalactions, ini_info.installtype, ini_info.buildtype)
+
+    def __init__(self):
+        print("Starting ><###> Tarpon Installer <###>< !!!")
+#        print("Using the following parameters: {} : {} : {}"
+#            .format(self.ini_info.host,self.ini_info.username,self.ini_info.password))
+
+
+def PrintHelp():
+    print("**********************************************************************")
+    print("*  ><###> Tarpon Installer <###>< is an open source install creator. *")
+    print("*  specify config.ini file on the commandline                        *")
+    print("*                                                                    *")
+    print("*  Usage: tarpon_installer.exe yourconfig.ini                        *")
+    print("*                                                                    *")
+    print("*  VERSION {}".format(version))
+    print("**********************************************************************")
+    raise SystemExit
+
+
+if __name__ == "__main__":
+    params = {}
+    option = ""
+    skipImport = False
+
+    if len(sys.argv) < 2:
+        PrintHelp()
+
+    for arg in sys.argv:
+        if arg[0:1] == "-":
+            option = arg
+        else:
+            params[option] = arg
+
+    for item in params:
+        configfile = params[item]
+        print("parameter {}".format(params[item]))
+
+    if path.exists(configfile) == False:
+        print("ERROR: ><###> Cannot Find Configuration File. <###><")
+        PrintHelp()
+    
+    mc = mainClass()
+    mc.main()
---
+++ task.py
@@ -0,0 +1,187 @@
+import time
+import paramiko
+import os
+import subprocess
+import shutil
+from zipfile import ZipFile
+from shutil import copyfile
+from pathlib import Path
+
+
+class Task:
+    ssh = paramiko.SSHClient()
+    username = ""
+    password = ""
+    hostname = ""
+    resources = ""
+
+    def loginSSH(self):
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh.connect(hostname = self.hostname, username = self.username, password = self.password)
+
+    def installRepo(self, resources, repo):
+        ftp = self.ssh.open_sftp()
+        for key in repo:
+            print('copying and extracting Repo files {} to {}'.format(key, repo[key]))
+            self.ssh.exec_command('mkdir {}'.format(repo[key]))
+            src = "{}/{}".format(resources, key)
+            dst = "{}/{}".format(repo[key],key)
+            ftp.put(src , dst)
+            stdin, stdout, stderr = self.ssh.exec_command('tar xvfz {}/{}'.format(repo[key],key))
+            for line in iter(stdout.readline,""):
+                print (line, end="")
+            stdin, stdout, stderr = self.ssh.exec_command('chmod 775 -f {}/{}'.format(repo[key],key))
+            for line in iter(stdout.readline,""):
+                print (line, end="")
+        ftp.close()
+
+    def installRPMs(self, resources, rpms):
+        for key in rpms:
+            print('install RPMs {} to {}'.format(key, rpms[key]))
+            stdin, stdout, stderr = self.ssh.exec_command('/root/installpkg.sh {}'.format(rpms[key]))
+            for line in iter(stdout.readline,""):
+                print (line, end="")
+            time.sleep(5)
+
+    def copyFromResourcesLocal(self, resources, files):
+        for key in files:
+            firstfile = 0
+            my_file = Path("{}/{}".format(resources,key))
+            if my_file.is_file():
+                print('copying and extracting file {} to {}'.format(key, files[key]))
+                try:
+                    os.makedirs(files[key], exist_ok = True)
+                    print("Directory {} created successfully".format(files[key]))
+                except OSError as error:
+                    print("Directory {} can not be created".format(files[key]))
+
+                src = "{}/{}".format(resources, key)
+                dst = "{}/{}".format(files[key],key)
+                copyfile(src,dst)
+
+                if 'zip' in key:
+
+                    with ZipFile(dst) as zf:
+                        hasdirectory = False  # the zip file has an initial directory in which files will be extracted
+                        for file in zf.namelist():
+
+                            if firstfile == 0:
+                                firstfile = 1
+                                test = file.split('/')
+                                if(len(test) > 1):
+                                    hasdirectory = True
+
+                            filename = os.path.basename(file)
+
+                            # create directories
+                            if not filename:
+                                words = file.split('/')
+                                filename = ""
+
+                                if(hasdirectory == False):
+                                    filename = file
+                                else:
+                                    for i in range(len(words)-1):
+                                        if i == 0:
+                                            filename += words[i+1]
+                                        else:
+                                            filename += "/"
+                                            filename += words[i+1]
+
+                                os.makedirs(files[key] +"/" + filename, exist_ok = True)
+                                continue
+    
+                            # copy file (taken from zipfile's extract)
+                            words = file.split('/')
+                            filename = ""
+
+                            if hasdirectory == False:
+                                filename = file
+                            else:
+                                if len(words) > 1:
+                                    for i in range(len(words)-1):
+                                        if i == 0:
+                                            filename += words[i+1]
+                                        else:
+                                            filename += "/"
+                                            filename += words[i+1]
+                                else:
+                                    filename = words[0]
+
+                            source = zf.open(file)
+                            target = open(os.path.join(files[key], filename), "wb")
+                            with source, target:
+                                shutil.copyfileobj(source, target)
+            else:
+                print("Error copying file {}/{}, it does not exist".format(resources,key))
+
+    def copyFromResourcesSSH(self, resources, files):
+        ftp = self.ssh.open_sftp()
+        for key in files:
+            my_file = Path("{}/{}".format(resources,key))
+            if my_file.is_file():
+                print('copying and extracting file {} to {}'.format(key, files[key]))
+                self.ssh.exec_command('mkdir {}'.format(files[key]))
+                src = "{}/{}".format(resources, key)
+                dst = "{}/{}".format(files[key],key)
+                ftp.put(src , dst)
+
+                if 'zip' in key:
+                    stdin, stdout, stderr = self.ssh.exec_command('unzip -o {}/{} -d {}'.format(files[key],key,files[key]))
+                    for line in iter(stdout.readline,""):
+                        print (line, end="")
+            else:
+                print("Error copying file {}/{}, it does not exist".format(resources,key))
+        ftp.close()
+
+    def copyFromResources(self, resources, files, installtype, buildtype):
+        if(installtype == 'REMOTE' and buildtype == 'LINUX'):
+            self.copyFromResourcesSSH(resources, files)
+        elif installtype == 'LOCAL':
+            self.copyFromResourcesLocal(resources, files)
+
+    def doActionsSSH(self, actions):
+        for action in actions:
+            if '%host%' in actions[action]:
+                actions[action] = actions[action].replace("%host%",self.hostname)
+            print('Executing Action {} with {}'.format(action, actions[action]))
+            stdin, stdout, stderr = self.ssh.exec_command('{}'.format(actions[action]))
+            for line in iter(stdout.readline,""):
+                print (line, end="")
+
+    def doActionsLocal(self, actions):
+        for action in actions:
+            try:
+                if '%host%' in actions[action]:
+                    actions[action] = actions[action].replace("%host%",self.hostname)
+
+                print('Executing Action {} with {}'.format(action, actions[action]))
+
+                p = subprocess.check_output(actions[action], shell=True)
+                for line in p.splitlines():
+                    print(line.decode('utf-8'))
+            except Exception as e:
+                print("Error in Action {}".format(str(e)))
+
+    def doActions(self, actions, installtype, buildtype):
+        if(installtype == 'REMOTE' and buildtype == 'LINUX'):
+            self.doActionsSSH(actions)
+        elif installtype == 'LOCAL':
+            self.doActionsLocal(actions)
+
+    def modifyFiles(self, files, buildtype):
+        for file in files:
+            result = files[file].split("||")
+            file = file.split("-",1)
+            stdin, stdout, stderr = self.ssh.exec_command('sed -i \"s/{}/{}/g\" {}'.format(result[0],result[1],file[1]))
+            for line in iter(stderr.readline,""):
+                print (line, end="")
+
+    def finalActions(self, actions, installtype, buildtype):
+        self.doActions(actions, installtype, buildtype)
+
+    def __init__(self, username, password, hostname, resources):
+        self.username = username
+        self.password = password
+        self.hostname = hostname    
+        self.resources = resources
