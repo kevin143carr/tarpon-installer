bcf4cfa7ca866e2a59c4feff86f9cf6932c530ad
kevin143carr@yahoo.com <kevin143carr@yahoo.com>
2024-05-17 13:46:27 -0400

    added folders and files

---
+++ managers/processmanager.py
@@ -0,0 +1,63 @@
+import time
+import paramiko
+import subprocess
+from fileutilities import FileUtilities
+from easygui import *
+from subprocess import PIPE
+import logging
+import threading
+
+class ProcessManager:
+    ssh = paramiko.SSHClient()
+    username = ""
+    password = ""
+    hostname = ""
+    resources = ""
+    file_utilities = FileUtilities
+    logger = logging.getLogger("logger")
+    lock = threading.Lock()
+    
+    def checkForWatchdogEvent(self,pid, action):
+        watchdogfile = open('tarpon_watchdog.log','r')
+        lines = watchdogfile.readlines()
+        
+        for line in lines:
+            if pid in line:
+                print("watchdog event occured - re-issueing command {}".format(action))
+                self.logger.error("watchdog event occured - re-issueing command in Action {}".format(action))
+                watchdogfile.close()
+                self.executeProcs(action, True)
+                
+        watchdogfile.close()    
+    
+    def executeProcsDebug(self, action, watchdog = False):
+        p = subprocess.Popen(action,shell=True, stdout=PIPE, stderr=PIPE, 
+                             start_new_session=True, encoding='utf-8')
+        self.logger.info("PID [{}] COMMAND [{}]".format(p.pid,action))
+        p.wait()
+        
+        time.sleep(2)
+
+        if watchdog == True:
+            pidval = "PID [{}]".format(p.pid)
+            self.checkForWatchdogEvent(pidval, action)
+        
+        stdout,stderr = p.communicate(timeout=180)
+        
+        for line in str(stdout).splitlines():
+            self.logger.debug(line)
+                
+        for line in str(stderr).splitlines():
+            self.logger.debug(line)
+            
+    def executeProcs(self, action, watchdog = False):
+        p = subprocess.Popen(action,shell=True, stdout=None, stderr=None, 
+                             start_new_session=False, encoding=None)
+        self.logger.info("PID [{}] COMMAND [{}]".format(p.pid,action))
+        p.wait()
+        
+        time.sleep(2)
+
+        if watchdog == True:
+            pidval = "PID [{}]".format(p.pid)
+            self.checkForWatchdogEvent(pidval, action)       
---
+++ managers/rpmmanager.py
@@ -0,0 +1,44 @@
+import time
+import subprocess
+from fileutilities import FileUtilities
+from processmanager import ProcessManager
+import logging
+
+class RpmManager:
+    file_utilities = FileUtilities
+    process_manager =  ProcessManager
+    logger = logging.getLogger("logger")
+    
+    def installRPMsRemote(self, resources, rpms):
+        for key in rpms:
+            self.logger.info('install RPMs {} to {}'.format(key, rpms[key]))
+            p = subprocess.check_output('/root/installpkg.sh {}'.format(rpms[key]))
+            for line in str(p).splitlines():
+                self.logger.info (line, end="")
+            time.sleep(5)
+            
+    def installLocalRPMs(self, window, bar, taskitem, resources, rpms, watchdog):
+        p = None
+        count = 0
+        for key in rpms:
+            count += 1;
+            bar['value'] = (count/len(rpms.keys()))*100
+            
+            self.logger.info('install RPMs {} {}'.format(key, rpms[key]))
+            try:
+                multilinerpm = rpms[key].split(",")
+                execstr = ''
+                for line in multilinerpm:
+                    if execstr == '':
+                        execstr = 'rpm --install {}/{} '.format(resources, line)
+                    else:
+                        execstr = execstr + '{}/{} '.format(resources, line)
+                        
+                taskstr = 'Installing RPM - {}'.format(execstr)
+                self.logger.info(taskstr)
+                taskitem.set(taskstr)
+                
+                self.process_manager.executeProcs(execstr, watchdog)
+                    
+            except Exception as ex:
+                self.logger.error("{} : file {}".format(ex,rpms[key]))    
